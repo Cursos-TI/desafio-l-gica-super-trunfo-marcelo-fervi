9cd3ded45adde0ac996325bc82d262027f5ae0aa
Marcelo Ferreira <35692178+marcelo-fervi@users.noreply.github.com>
2025-02-14 16:21:43 +0000

    Progresso no Desafio Aventureiro

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,3 +1,7 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS 1 
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 1 
+
 #include <stdio.h>
 
 // Desafio Super Trunfo - Países
@@ -108,9 +112,120 @@ void desafioNovato() {
     compararPropriedadeGeralEInformarCidadeGanhadora("Superpoder", carta1SuperPoder, carta2SuperPoder, carta1Nome, carta2Nome);*/
 }
 
+
+
+
+
+
+//Função válida para todas as propriedades, MENOS Densidade Populacional.
+void compararPropriedadeGeralEInformarCidadeGanhadora_Aventureiro(const char* propriedadeNome, float carta1Valor, float carta2Valor, char* carta1NomeDaCidade, char* carta2NomeDaCidade) {
+    printf("Resultado da comparação de %s entre as cidades '%s' e '%s':\n", propriedadeNome, carta1NomeDaCidade, carta2NomeDaCidade);
+
+    if (carta1Valor > carta2Valor) {
+        printf("Vencedor: '%s' devido a %f ser maior do que %f.\n", carta1NomeDaCidade, carta1Valor, carta2Valor);
+    }
+    else if (carta2Valor > carta1Valor) {
+        printf("Vencedor: '%s' devido a %f ser maior do que %f.\n", carta2NomeDaCidade, carta2Valor, carta1Valor);
+    }
+    else {
+        printf("Vencedor: Empate devido a %f ser igual a %f.\n", carta1Valor, carta2Valor);
+    }
+}
+
+//Função válida SOMENTE para a propriedade Densidade Populacional.
+void compararDensidadePopulacionalEInformarCidadeGanhadora_Aventureiro(const char* propriedadeNome, float carta1Valor, float carta2Valor, char* carta1NomeDaCidade, char* carta2NomeDaCidade) {
+    printf("Resultado da comparação de %s entre as cidades '%s' e '%s':\n", propriedadeNome, carta1NomeDaCidade, carta2NomeDaCidade);
+
+    if (carta1Valor > carta2Valor) {
+        printf("Vencedor: '%s' devido a %f ser menor do que %f.\n", carta2NomeDaCidade, carta2Valor, carta1Valor);
+    }
+    else if (carta2Valor > carta1Valor) {
+        printf("Vencedor: '%s' devido a %f ser menor do que %f.\n", carta1NomeDaCidade, carta1Valor, carta2Valor);
+    }
+    else {
+        printf("Vencedor: Empate devido a %f ser igual a %f.\n", carta1Valor, carta2Valor);
+    }
+}
+
+int perguntarPropriedadeASerComparada() {
+    printf("Informe qual o número da propriedade que deseja comparar entre as cartas, conforme as opções a seguir:\n");
+    printf("1. População\n");
+    printf("2. Área\n");
+    printf("3. Pontos turísticos\n");
+    printf("4. Densidade populacional\n");
+    printf("5. PIB\n");
+    printf("6. PIB Per Capita\n");
+    printf("7. Superpoder\n");
+
+    int propriedadeEscolhida;
+    scanf("%d", &propriedadeEscolhida);
+
+    return propriedadeEscolhida;
+}
+
+/*
+    Duas cartas serão construídas pelo usuário e suas informações serão imprimidas após a construção de ambas.
+    Então, o usuário deverá selecionar qual propriedade ele deseja comparar entre as cartas para determinar a carta vencedora.
+*/
+void desafioAventureiro() {
+    printf("Para começarmos, informe os dados das cidades conforme será solicitado a seguir:\n");
+
+    char* carta1Codigo = new char[5];
+    char* carta1Nome = new char[32];
+    int carta1Populacao, carta1Area, carta1Pib, carta1PontosTuristicos;
+    float carta1DensidadePopulacional, carta1PibPerCapita, carta1SuperPoder;
+    printf("- Primeira cidade\n");
+    perguntarDadosDaCidade(carta1Codigo, carta1Nome, carta1Populacao, carta1Area, carta1Pib, carta1PontosTuristicos, carta1DensidadePopulacional, carta1PibPerCapita, carta1SuperPoder);
+
+    printf("\n");
+
+    char* carta2Codigo = new char[5];
+    char* carta2Nome = new char[32];
+    int carta2Populacao, carta2Area, carta2Pib, carta2PontosTuristicos;
+    float carta2DensidadePopulacional, carta2PibPerCapita, carta2SuperPoder;
+    printf("- Segunda cidade\n");
+    perguntarDadosDaCidade(carta2Codigo, carta2Nome, carta2Populacao, carta2Area, carta2Pib, carta2PontosTuristicos, carta2DensidadePopulacional, carta2PibPerCapita, carta2SuperPoder);
+
+    printf("\n");
+
+    imprimirDadosDaCidade(carta1Codigo, carta1Nome, carta1Populacao, carta1Area, carta1Pib, carta1PontosTuristicos, carta1DensidadePopulacional, carta1PibPerCapita, carta1SuperPoder);
+    printf("\n");
+    imprimirDadosDaCidade(carta2Codigo, carta2Nome, carta2Populacao, carta2Area, carta2Pib, carta2PontosTuristicos, carta2DensidadePopulacional, carta2PibPerCapita, carta2SuperPoder);
+
+    printf("\n");
+
+    // Pergunta ao usuário qual a propriedade ele deseja usar para a comparação das cartas.
+    int propriedadeParaComparar = perguntarPropriedadeASerComparada();
+
+    // Exibe o resultado final da comparação escolhida:
+    switch (propriedadeParaComparar) {
+        case 1: //População
+            compararPropriedadeGeralEInformarCidadeGanhadora_Aventureiro("Tamanho da população", carta1Populacao, carta2Populacao, carta1Nome, carta2Nome);
+            break;
+        case 2: //Área
+            compararPropriedadeGeralEInformarCidadeGanhadora_Aventureiro("Tamanho da área", carta1Area, carta2Area, carta1Nome, carta2Nome);
+            break;
+        case 3: //Pontos turísticos
+            compararPropriedadeGeralEInformarCidadeGanhadora_Aventureiro("Total de pontos turísticos", carta1PontosTuristicos, carta2PontosTuristicos, carta1Nome, carta2Nome);
+            break;
+        case 4: //Densidade populacional
+            compararDensidadePopulacionalEInformarCidadeGanhadora_Aventureiro("Densidade populacional", carta1DensidadePopulacional, carta2DensidadePopulacional, carta1Nome, carta2Nome);
+            break;
+        case 5: //PIB
+            compararPropriedadeGeralEInformarCidadeGanhadora_Aventureiro("PIB total", carta1Pib, carta2Pib, carta1Nome, carta2Nome);
+            break;
+        case 6: //PIB per Capita
+            compararPropriedadeGeralEInformarCidadeGanhadora_Aventureiro("PIB per Capita", carta1PibPerCapita, carta2PibPerCapita, carta1Nome, carta2Nome);
+            break;
+        case 7: //Superpoder
+            compararPropriedadeGeralEInformarCidadeGanhadora_Aventureiro("Superpoder", carta1SuperPoder, carta2SuperPoder, carta1Nome, carta2Nome);
+            break;
+    }
+}
+
 int main() {
-    desafioNovato();
-    //desafioAventureiro();
+    //desafioNovato();
+    desafioAventureiro();
     //desafioMestre();
 
     return 0;
